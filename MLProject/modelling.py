ebb8294e0a7496331147d3e78ea3bef6c40b4773
Reyfaldho Alfarazel <144695357+Reyfaldho@users.noreply.github.com>
2025-12-20 19:34:46 +0800

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,6 +1,3 @@
-import os
-os.environ["MPLBACKEND"] = "Agg"
-
 import argparse
 import pandas as pd
 import mlflow
@@ -8,45 +5,41 @@ import mlflow.sklearn
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
-
+from sklearn.metrics import accuracy_score
 
 def main():
-    p = argparse.ArgumentParser()
-    p.add_argument("--data_path", type=str, required=True)
-    p.add_argument("--target_col", type=str, default="target")
-    p.add_argument("--experiment_name", type=str, default="CI_Retraining_HeartDisease")
-    args = p.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_path", type=str, required=True)
+    parser.add_argument("--target_col", type=str, required=True)
+    args = parser.parse_args()
 
-    mlflow.set_experiment(args.experiment_name)
+    mlflow.set_experiment("CI_Retraining_HeartDisease")
 
     df = pd.read_csv(args.data_path)
-    X = df.drop(columns=[args.target_col, "num_original"], errors="ignore").astype("float64")
-    y = df[args.target_col].astype(int)
+
+    X = df.drop(columns=[args.target_col])
+    y = df[args.target_col]
 
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42, stratify=y
+        X, y, test_size=0.2, random_state=42
     )
 
-    model = RandomForestClassifier(
-        n_estimators=300,
-        random_state=42,
-        n_jobs=-1,
-        class_weight="balanced",
-    )
+    model = RandomForestClassifier(random_state=42)
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
-    y_proba = model.predict_proba(X_test)[:, 1]
+    acc = accuracy_score(y_test, y_pred)
 
-    mlflow.log_metric("accuracy", accuracy_score(y_test, y_pred))
-    mlflow.log_metric("precision", precision_score(y_test, y_pred, zero_division=0))
-    mlflow.log_metric("recall", recall_score(y_test, y_pred, zero_division=0))
-    mlflow.log_metric("f1", f1_score(y_test, y_pred, zero_division=0))
-    mlflow.log_metric("roc_auc", roc_auc_score(y_test, y_proba))
+    mlflow.log_metric("accuracy", acc)
+    mlflow.log_param("model_type", "RandomForest")
 
-    mlflow.sklearn.log_model(model, artifact_path="model")
+    mlflow.sklearn.log_model(
+        model,
+        artifact_path="model",
+        registered_model_name="HeartDiseaseModel"
+    )
 
+    print(f"Training finished. Accuracy: {acc}")
 
 if __name__ == "__main__":
     main()
